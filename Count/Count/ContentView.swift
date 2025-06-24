92c83a6e2ac1dc7c580bbfcefafbeec4e5bc3474
gil <ryogo5.0304@gmail.com>
2025-06-24 10:37:04 +0900

    count

---
+++ Count/Count/ContentView.swift
@@ -0,0 +1,61 @@
+//
+//  ContentView.swift
+//  Count
+//
+//  Created by 落合遼梧 on 2025/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  
+  @State var count = 0
+  @State var result = ""
+  
+  var body: some View {
+    VStack {
+      Text("\(result)")
+        .frame(width: 300, height: 100)
+        .font(.system(size: 32))
+      Text("\(count)")
+        .frame(width: 300, height: 100)
+        .font(.system(size: 48))
+        .foregroundColor(count >= 0 ? .red : .blue)
+      HStack {
+        Button("+") {
+          count += 1
+          if count >= 5 {
+            result = "プラスの勝ち！"
+          } else if count <= -5 {
+            result = "マイナスの勝ち！"
+          } else {
+            result = ""
+          }
+        }
+        .font(.system(size: 48))
+        .foregroundStyle(.white)
+        .frame(width: 100, height: 100)
+        .background(.red)
+        Button("-") {
+          count -= 1
+          if count >= 5 {
+            result = "プラスの勝ち！"
+          } else if count <= -5 {
+            result = "マイナスの勝ち！"
+          } else {
+            result = ""
+          }
+        }
+        .font(.system(size: 48))
+        .foregroundStyle(.white)
+        .frame(width: 100, height: 100)
+        .background(.blue)
+      }
+      .padding()
+    }
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ Count/Count/CountApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountApp.swift
+//  Count
+//
+//  Created by 落合遼梧 on 2025/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
