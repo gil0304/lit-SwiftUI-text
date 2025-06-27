332af95aa5e7f09010655be0dea53a914d145634
gil <ryogo5.0304@gmail.com>
2025-06-27 23:20:36 +0900

    timer

---
+++ StopWatch/StopWatch/ContentView.swift
@@ -0,0 +1,82 @@
+//
+//  ContentView.swift
+//  StopWatch
+//
+//  Created by 落合遼梧 on 2025/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  
+  @State private var timer: Timer!
+  @State private var secondsElapsed: Double = 0.0
+  @State private var isRunning = false
+  
+  var body: some View {
+    VStack {
+      Text(String(format: "%.2f", secondsElapsed))
+        .font(.title)
+      HStack {
+        if isRunning {
+          Button {
+            pause()
+          } label: {
+            Image(systemName: "pause.fill")
+              .foregroundColor(.white)
+              .font(.title)
+              .padding()
+              .background(Color.green)
+              .clipShape(.circle)
+          }
+        } else {
+          Button {
+            start()
+          } label: {
+            Image(systemName: "play.fill")
+              .foregroundColor(.white)
+              .font(.title)
+              .padding()
+              .background(Color.green)
+              .clipShape(.circle)
+          }
+        }
+        if secondsElapsed != 0.0 {
+          Button {
+            stop()
+          } label: {
+            Image(systemName: "stop.fill")
+              .foregroundColor(.white)
+              .font(.title)
+              .padding()
+              .background(Color.red)
+              .clipShape(.circle)
+          }
+        }
+      }
+    }
+  }
+  
+  func start() {
+    timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+      secondsElapsed += 0.1
+    }
+    isRunning = true
+  }
+  
+  func pause() {
+    timer.invalidate()
+    isRunning = false
+  }
+  
+  func stop() {
+    timer.invalidate()
+    isRunning = false
+    secondsElapsed = 0.0
+  }
+  
+}
+
+#Preview {
+  ContentView()
+}
