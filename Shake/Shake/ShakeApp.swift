0ced766b6974252704f69cd81089e7ffcbfc2462
gil <ryogo5.0304@gmail.com>
2025-06-29 19:44:51 +0900

    Shake

---
+++ Shake/Shake/ShakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakeApp.swift
+//  Shake
+//
+//  Created by 落合遼梧 on 2025/06/29.
+//
+
+import SwiftUI
+
+@main
+struct ShakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            StartView()
+        }
+    }
+}
