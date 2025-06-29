fbd1f1d38751725ddfc6023836b4b80598a0b87e
gil <ryogo5.0304@gmail.com>
2025-06-29 19:13:41 +0900

    PhotoMaster

---
+++ PhotoMaster/PhotoMaster/PhotoMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMasterApp.swift
+//  PhotoMaster
+//
+//  Created by 落合遼梧 on 2025/06/29.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
